05f96cdfd02f52e4f2add9cc0ae8b8700acc616e
Apple <apple@MacBook-Pro.local>
2022-06-19 15:44:28 +0300

    1-module-1,2,3,4,5-tasks

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -9,8 +9,12 @@ function print(text) {
  * Эту функцию нужно поменять так,
  * чтобы функция sayHello работала корректно
  */
-function isValid(name) {
-  // ваш код...
+ function isValid(name) {
+  if (!name || name.includes (' ') || name.length < 4) {
+    return false;
+  } else {
+    return true;
+  }
 }
 
 function sayHello() {
@@ -22,3 +26,5 @@ function sayHello() {
     print('Некорректное имя');
   }
 }
+
+sayHello();
